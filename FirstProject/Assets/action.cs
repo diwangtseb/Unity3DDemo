e6f9d18e176624ea7b9a2a1e5bc9a2f16f7b8524
diwang <diwang839639311@163.com>
2021-08-23 21:19:47 +0800

    first commit

---
+++ FirstProject/Assets/action.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class action : MonoBehaviour
+{
+    private CharacterController car;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GetComponent<CharacterController>();
+        player = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = Vector3.zero;
+        move.x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        move.z = Input.GetAxis("Vertical") * Time.deltaTime;
+        move.y -= 3f * Time.deltaTime;
+        car.Move(player.TransformDirection(move));
+    }
+}
